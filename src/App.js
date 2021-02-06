32a9c2204105b9fcbcd03e2adc2d037bcaef89fb
Zemen Teshome <zementeshome@gmail.com>
2021-02-05 21:20:20 -0500

    rotation for box working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, { useRef } from 'react';
 import './App.scss';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
+// import { Box } from "drei";
+
+const Box = () => {
+  const mesh = useRef(null)
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return (
+    <mesh ref={mesh}>
+          {/* <circleBufferGeometry attach='geometry' args={[2,200]} /> */}
+          <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+          <meshStandardMaterial attach='material' />
+        </mesh>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      {/* <h1>Hello</h1>  */}
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-          <meshStandardMaterial attach='material' />
-        </mesh>
+        <Box />
+        {/* <Box>
+          <meshStandardMaterial attach='material'/>
+        </Box> */}
       </Canvas>
     </div>
   );
