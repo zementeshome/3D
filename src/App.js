8b10260328d1dcf591de8dc4a9a51ca5e71d411e
Zemen Teshome <zementeshome@gmail.com>
2021-02-07 22:38:36 -0500

    3 spinning cubes on page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import './App.scss';
 import { Canvas, useFrame } from 'react-three-fiber';
 // import { Box } from "drei";
 
-const Box = () => {
+const SpinningMesh = ({position}) => {
   const mesh = useRef(null)
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
           {/* <circleBufferGeometry attach='geometry' args={[2,200]} /> */}
           <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-          <meshStandardMaterial attach='material' />
+          <meshStandardMaterial attach='material' color='pink'/>
         </mesh>
   )
 }
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1>Hello</h1>  */}
-      <Canvas>
-        <Box />
+    <div className="app">
+      <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+        <ambientLight intensity={0.3} />
+        <SpinningMesh position={[0, 1, 0]}/>
+        <SpinningMesh position={[-2, 1, -5]}/>
+        <SpinningMesh position={[5, 1, -2]}/>
         {/* <Box>
           <meshStandardMaterial attach='material'/>
         </Box> */}
