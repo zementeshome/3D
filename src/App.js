d81070a3c9dcf6a1196ba588f78dfc3e77de8347
Zemen Teshome <zementeshome@gmail.com>
2021-02-10 21:25:50 -0500

    added skylight and working with plane

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import './App.scss';
 import { Canvas, useFrame } from 'react-three-fiber';
 // import { Box } from "drei";
-import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei';
+import { softShadows, MeshWobbleMaterial, OrbitControls, Stars } from '@react-three/drei';
 import { useSpring, animated } from 'react-spring/three';
 
 softShadows();
@@ -27,13 +27,24 @@ const SpinningMesh = ({position, args, color, speed}) => {
           <boxBufferGeometry attach='geometry' args={args}/>
           <MeshWobbleMaterial attach='material' color={color} speed={speed} factor={0.6}/>
         </animated.mesh>
-  )
+  );
+}
+
+const Plane = () => {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+    <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+    <meshLambertMaterial attach="material" color="white"/>
+  </mesh>
+  );
 }
 
 function App() {
   return (
     <div className="app">
       <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+        <Stars />
+        <spotLight position={[10,15, 10]} angle={0.3} />
         <ambientLight intensity={0.3}/>
         <directionalLight 
         castShadow
@@ -60,10 +71,15 @@ function App() {
         <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' speed={2}/>
         <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6}/>
         <SpinningMesh position={[5, 1, -2]}  color="pink" speed={6}/>
+        
+        {/* <SpinningMesh position={[-5, 2, 0]} args={[3, 2, 1]} color='lightblue' speed={2}/>
+        <SpinningMesh position={[3, 2, -10]} color="pink" speed={6}/>
+        <SpinningMesh position={[10, 2, 3]}  color="pink" speed={6}/> */}
         {/* <Box>
           <meshStandardMaterial attach='material'/>
         </Box> */}
         <OrbitControls />
+        {/* <Plane/> */}
       </Canvas>
     </div>
   );
